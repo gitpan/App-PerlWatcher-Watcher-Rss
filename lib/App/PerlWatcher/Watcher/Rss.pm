56a9d558ed89fe6076bbb695971e45c94e225f26
Ivan Baidakou <dmol@cpan.org>
2013-11-16 15:47:56 +0000

    Import of DMOL/App-PerlWatcher-Watcher-Rss-0.19 from CPAN.
    gitpan-cpan-distribution: App-PerlWatcher-Watcher-Rss
    gitpan-cpan-version:      0.19
    gitpan-cpan-path:         DMOL/App-PerlWatcher-Watcher-Rss-0.19.tar.gz
    gitpan-cpan-author:       DMOL
    gitpan-cpan-maturity:     released

--- lib/App/PerlWatcher/Watcher/Rss.pm
+++ lib/App/PerlWatcher/Watcher/Rss.pm
@@ -1,6 +1,6 @@
 package App::PerlWatcher::Watcher::Rss;
 {
-  $App::PerlWatcher::Watcher::Rss::VERSION = '0.18';
+  $App::PerlWatcher::Watcher::Rss::VERSION = '0.19';
 }
 # ABSTRACT: Watches RSS feed and returns last news headers as clickable Eventitems.
 
@@ -73,7 +73,7 @@ App::PerlWatcher::Watcher::Rss - Watches RSS feed and returns last news headers 
 
 =head1 VERSION
 
-version 0.18
+version 0.19
 
 =head1 SYNOPSIS
 
